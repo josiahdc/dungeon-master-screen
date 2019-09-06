a5716fbcc58a01731403e7cf343029eef3a43d2c
school <josiahc@uw.edu>
2019-09-06 13:35:30 -0700

    added grid view

--- src/components/monsters/MonsterPage.js
+++ src/components/monsters/MonsterPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './MonsterPage.css';
 import { MonsterMenu } from './MonsterMenu';
 import { MonsterStatBlockRegion } from './MonsterStatBlockRegion';
+import Cookies from 'js-cookie';
 import Box from '@material-ui/core/Box';
 const monsterNames = require('./../../monsterstats/monsterNames.json')
 const monsterList = require('./../../monsterstats/monsters.json')
@@ -10,15 +11,45 @@ const monsterList = require('./../../monsterstats/monsters.json')
 export class MonsterPage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { activeMonsters: ["Owlbear", "Cultist"] }
+        let activeMonsterCookie = Cookies.get("activeMonsters");
+        let activeMonsters = [];
+        let layout = [];
+        if (activeMonsterCookie !== undefined) {
+            activeMonsters = JSON.parse(activeMonsterCookie);
+            layout = JSON.parse(Cookies.get("layout"));
+        }
+        this.state = {
+            "activeMonsters": activeMonsters,
+            "layout": layout
+        }
+
+        this.calculateLayout = this.calculateLayout.bind(this);
         this.removeActive = this.removeActive.bind(this);
         this.addActive = this.addActive.bind(this);
+        this.onLayoutChange = this.onLayoutChange.bind(this);
+    }
+
+    calculateLayout(activeMonsters, oldLayout) {
+        let newLayout = oldLayout.slice(0);
+        let existingMonsters = [];
+        oldLayout.forEach((item) => {
+            existingMonsters.push(item.i);
+        })
+        let newMonsters = activeMonsters.filter((monster) => { return !existingMonsters.includes(monster) })
+        newMonsters.forEach((monsterName, index) => {
+            newLayout.push({ i: monsterName, x: 0, y: Infinity, w: 30, h: 35 });
+        });
+        return newLayout;
     }
 
     addActive(monsterName) {
         if (this.state.activeMonsters.indexOf(monsterName) === -1) {
             let newActiveMonsters = this.state.activeMonsters.concat([monsterName]);
-            this.setState({activeMonsters: newActiveMonsters});
+            Cookies.set("activeMonsters", JSON.stringify(newActiveMonsters));
+            this.setState({
+                activeMonsters: newActiveMonsters,
+                layout: this.calculateLayout(newActiveMonsters, this.state.layout)
+            });
         }
     }
 
@@ -26,7 +57,15 @@ export class MonsterPage extends React.Component {
         let newActiveMonsters = this.state.activeMonsters.filter((monsterInState) => {
             return monsterInState !== monsterName;
         });
-        this.setState({ activeMonsters: newActiveMonsters });
+        Cookies.set("activeMonsters", JSON.stringify(newActiveMonsters));
+        this.setState({
+            activeMonsters: newActiveMonsters
+        });
+    }
+
+    onLayoutChange(layout) {
+        this.setState({ "layout": layout });
+        Cookies.set("layout", JSON.stringify(layout));
     }
 
     render() {
@@ -36,7 +75,9 @@ export class MonsterPage extends React.Component {
                     monsterNames={monsterNames} />
                 <MonsterStatBlockRegion activeMonsters={this.state.activeMonsters}
                     removeMonsterFunction={this.removeActive}
-                    monsterList={monsterList} />
+                    monsterList={monsterList}
+                    monsterlayout={this.state.layout}
+                    layoutChange={this.onLayoutChange} />
             </Box>
         );
     }
--- src/components/monsters/MonsterStatBlockWrapper.js
+++ src/components/monsters/MonsterStatBlockWrapper.js
@@ -2,32 +2,23 @@ import React from 'react';
 import './MonsterStatBlockWrapper.css';
 import { MonsterStatBlock } from './MonsterStatBlock';
 import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
 
 
 export class MonsterStatBlockWrapper extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleRemoveButtonClick = this.handleRemoveButtonClick.bind(this);
-    }
-
-    handleRemoveButtonClick() {
-        this.props.removeMonsterFunction(this.props.monster);
-    }
-
     render() {
         return (
             <Box className="MonsterStatBlockWrapper">
-                <Box className="gold-border">
-                    <Box className="color-overlay"></Box>
+                <Box className="monster-card">
+                    <Box className="gold-border">
+                        <Box className="color-overlay"></Box>
+                    </Box>
+                    <MonsterStatBlock monster={this.props.monster}
+                        monsterList={this.props.monsterList} />
+                    <Box className="gold-border">
+                        <Box className="color-overlay"></Box>
+                    </Box>
                 </Box>
-                <MonsterStatBlock monster={this.props.monster}
-                    monsterList={this.props.monsterList} />
-                <Box className="gold-border">
-                    <Box className="color-overlay"></Box>
-                </Box>
-                <button onClick={this.handleRemoveButtonClick}>
-                    Remove
-                </button>
             </Box>
         );
     }
